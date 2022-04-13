75204a0a30a9358716deec99b8c27b61cccc2b54
dogyeong <dogyeong@zuminternet.com>
2022-04-13 17:31:25 +0900

    feat: web-push 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,27 @@
-import {ZumWebPush} from '@zum-front-core/web-push'
+import { ZumWebPush } from '@zum-front-core/web-push';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzkR_2nqrTsqKIg7Hp0nXhlw_jNV2fXk8",
-  authDomain: "zum-app-d6b3d.firebaseapp.com",
-  databaseURL: "https://zum-app-d6b3d.firebaseio.com",
-  projectId: "zum-app-d6b3d",
-  storageBucket: "zum-app-d6b3d.appspot.com",
-  messagingSenderId: "1054822071827",
-  appId: "1:1054822071827:web:ee64270513e07488ac09e6",
-  measurementId: "G-7FJYR6590G"
+  apiKey: 'AIzaSyBzkR_2nqrTsqKIg7Hp0nXhlw_jNV2fXk8',
+  authDomain: 'zum-app-d6b3d.firebaseapp.com',
+  databaseURL: 'https://zum-app-d6b3d.firebaseio.com',
+  projectId: 'zum-app-d6b3d',
+  storageBucket: 'zum-app-d6b3d.appspot.com',
+  messagingSenderId: '1054822071827',
+  appId: '1:1054822071827:web:ee64270513e07488ac09e6',
+  measurementId: 'G-7FJYR6590G',
 };
 
 const onBackgroundMessageHandler = (payload) => {
-  console.log(payload)
-  const {title, body} = payload.notification;
+  console.log(payload);
+  const { title, body } = payload.notification;
   const notificationTitle = title;
   const notificationOptions = {
     body,
   };
 
-  self.registration?.showNotification(notificationTitle,
-    notificationOptions);
-}
+  self.registration?.showNotification(notificationTitle, notificationOptions);
+};
 
-ZumWebPush.onBackgroundMessage({firebaseConfig})
+ZumWebPush.overrideOnClickNotification();
+
+ZumWebPush.onBackgroundMessage({ firebaseConfig });
--- public/index.js
+++ public/index.js
@@ -1,24 +1,24 @@
-import { ZumWebPush } from '@zum-front-core/web-push'
+import { ZumWebPush } from '@zum-front-core/web-push';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzkR_2nqrTsqKIg7Hp0nXhlw_jNV2fXk8",
-  authDomain: "zum-app-d6b3d.firebaseapp.com",
-  databaseURL: "https://zum-app-d6b3d.firebaseio.com",
-  projectId: "zum-app-d6b3d",
-  storageBucket: "zum-app-d6b3d.appspot.com",
-  messagingSenderId: "1054822071827",
-  appId: "1:1054822071827:web:ee64270513e07488ac09e6",
-  measurementId: "G-7FJYR6590G"
+  apiKey: 'AIzaSyBzkR_2nqrTsqKIg7Hp0nXhlw_jNV2fXk8',
+  authDomain: 'zum-app-d6b3d.firebaseapp.com',
+  databaseURL: 'https://zum-app-d6b3d.firebaseio.com',
+  projectId: 'zum-app-d6b3d',
+  storageBucket: 'zum-app-d6b3d.appspot.com',
+  messagingSenderId: '1054822071827',
+  appId: '1:1054822071827:web:ee64270513e07488ac09e6',
+  measurementId: 'G-7FJYR6590G',
 };
 
-const vapidKey = 'BFvqxAtemDbyY2R-PqfgjV-GTavbsZojGR2z5GERMog_twbXN5lWKpZXXlHvWbTFbOo2jMYmmb1zARLwI7sE6Z8'
+const vapidKey = 'BFvqxAtemDbyY2R-PqfgjV-GTavbsZojGR2z5GERMog_twbXN5lWKpZXXlHvWbTFbOo2jMYmmb1zARLwI7sE6Z8';
 
 const onPermissionGranted = (token) => {
-  console.log(token)
-}
+  console.log(token);
+};
 
 const onMessageHandler = (payload) => {
   new Notification(payload.notification?.title || '');
-}
+};
 
-ZumWebPush.onForegroundMessage({ firebaseConfig, vapidKey, onPermissionGranted })
+ZumWebPush.onForegroundMessage({ firebaseConfig, vapidKey, onPermissionGranted });
